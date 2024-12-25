e927ba1f8cdff000168e8828cbc8e0d61372bcc7
ZenSnorlax <q1510017673@outlook.com>
2024-12-25 10:59:42 +0800

    更改了 检查规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,12 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
-    "@typescript-eslint/no-explicit-any": "off", // 或调低为 "warn"
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn", // 将错误级别调低为警告
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // 调低为警告
+      { argsIgnorePattern: "^_" }, // 忽略下划线开头的参数
+    ],
+    "no-redeclare": "warn", // 禁止变量重新声明，调低为警告
+    "no-undef": "warn", // 禁止未定义变量的使用，调低为警告
   },
 };
